5fad37064e6868c74654f20de081a68eb71eb612
ZhuHaizhen <32768294+ZhuHaizhen@users.noreply.github.com>
2017-10-19 11:11:09 +0800

    initial version

--- heatmap.r
+++ heatmap.r
@@ -1,16 +1,8 @@
-args<-commandArgs(T)
-if(length(args)<2){
-print('Rscript heatmap.r rpkm.xls heatmap.pdf')
-print('length of args wrong!')
-q()
-}
-data<-args[1]
-out<-args[2]
-library(gplots)
-dt<-read.table(data,header=T,row.names=1,check.names=F,stringsAsFactors=F)
-pdf(out,width=12,height=10)
-#heatmap(as.matrix(dt),labRow=NA,margins = c(15, 5),cexCol=1,col=colorRampPalette(rev(c("#F2D732","#000000","#0059D5")))(20))
-#heatmap.2(as.matrix(dt),margins = c(17, 5),col=colorRampPalette(rev(c("red","black","green")))(20),trace="none",scale='row')
-heatmap.2(as.matrix(dt),margins = c(5, 20),col=colorRampPalette(rev(c("red","black","green")))(20),trace="none",scale='column',cexRow=1.2,xlab="Samples",cex.lab=5,col.lab="blue")
-
-dev.off()
+library(readr)
+library(tidyr)
+library(ggplot2)
+dataset <- read_csv("E:/lab/A_Cave/raw_data_Zhang/171017/L2.csv")
+phylum<-gather(dataset, key = Cave, value = Abundance, - Phylum)
+p<-ggplot(phylum,aes(x=Cave, y=Phylum),labs(x= "Cave", y= "Phylum")) + theme_classic() + geom_tile(aes(fill=Abundance)) + theme(axis.text.x = element_text(angle = 45,hjust = 1,vjust = 1)) +theme(axis.text.y = element_text(face="italic")) + theme(axis.text.y = element_text(size = 8))  +theme(axis.title = element_text(size = 10)) + theme(legend.title = element_text(size = 10))+ scale_fill_gradient(low = "lightskyblue",high = "lightsteelblue4")
+p
+ggsave(p,filename = "heatmap.pdf",width = 10, height = 10,units = c("cm"))
