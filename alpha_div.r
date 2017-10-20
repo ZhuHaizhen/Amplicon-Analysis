349158b6e3df8760e7c141913254cc4837a2997c
ZhuHaizhen <32768294+ZhuHaizhen@users.noreply.github.com>
2017-10-20 10:29:43 +0800

    initial version

---
+++ alpha_div.r
@@ -0,0 +1,9 @@
+library(ggplot2)
+library(tidyr)
+library(gridExtra)
+alpha = read.table("alpha_facet.txt", header=T, row.names= 1, sep="\t")
+alpha_facet = gather(alpha,key = diversity, value = indices, -Cave)
+theme_set(theme_bw())
+p = ggplot(alpha_facet, aes(x=Cave, y=indices, color=Cave)) + geom_boxplot(alpha=1, outlier.size=0, size=0.7, width=0.5, fill="transparent") +  geom_jitter( position=position_jitter(0.17), size=1, alpha=0.7) + labs(x="Cave", y="Diversity indices") + theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1)) + facet_wrap(~ diversity,scales = "free", ncol = 2, labeller = )
+p
+ggsave(p,filename = "alpha diversity.pdf")
