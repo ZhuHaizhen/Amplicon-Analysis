633a8188faf53474b8363b13d2ece382c2cbb70a
ZhuHaizhen <32768294+ZhuHaizhen@users.noreply.github.com>
2017-11-15 07:37:44 +0800

    171115

--- beta_diversity.r
+++ beta_diversity.r
@@ -16,7 +16,7 @@ points.w <- as.data.frame(pcoa.w$points)
 colnames(points.w) <- c("x", "y", "z") 
 eig.w <- pcoa.w$eig
 points.w <- cbind(points.w, design.in[match(rownames(points.w), rownames(design.in)), ])
-p1 <- ggplot(points.w,aes(x = x,y = y, color = cave, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.9, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.w[1] / sum(eig.w), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.w[2] / sum(eig.w), digits=4), "%)", sep=""),title = "PCoA plot of weighted unifrac distance")
+p1 <- ggplot(points.w,aes(x = x,y = y, color = cave, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.8, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.w[1] / sum(eig.w), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.w[2] / sum(eig.w), digits=4), "%)", sep=""),title = "PCoA plot of weighted unifrac distance")
 
 idx.cu <- colnames(unweighted)%in%rownames(design.in)
 idx.ru <- rownames(unweighted)%in%rownames(design.in)
@@ -26,7 +26,7 @@ points.u <- as.data.frame(pcoa.u$points)
 colnames(points.u) <- c("x", "y", "z") 
 eig.u <- pcoa.u$eig
 points.u <- cbind(points.u, design.in[match(rownames(points.u), rownames(design.in)), ])
-p2 <- ggplot(points.u,aes(x = x,y = y, color = cave, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.9, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.u[1] / sum(eig.u), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.u[2] / sum(eig.u), digits=4), "%)", sep=""),title = "PCoA plot of unweighted unifrac distance")
+p2 <- ggplot(points.u,aes(x = x,y = y, color = cave, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.8, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.u[1] / sum(eig.u), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.u[2] / sum(eig.u), digits=4), "%)", sep=""),title = "PCoA plot of unweighted unifrac distance")
 
 idx.cb <- colnames(bray)%in%rownames(design.in)
 idx.rb <- rownames(bray)%in%rownames(design.in)
@@ -36,7 +36,7 @@ points.b <- as.data.frame(pcoa.b$points)
 colnames(points.b) <- c("x", "y", "z") 
 eig.b <- pcoa.b$eig
 points.b <- cbind(points.b, design.in[match(rownames(points.b), rownames(design.in)), ])
-p3 <- ggplot(points.b,aes(x = x,y = y, color = cave, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.9, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.b[1] / sum(eig.b), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.b[2] / sum(eig.b), digits=4), "%)", sep=""),title = "PCoA plot of bray curtis distance")
+p3 <- ggplot(points.b,aes(x = x,y = y, color = cave, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.8, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.b[1] / sum(eig.b), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.b[2] / sum(eig.b), digits=4), "%)", sep=""),title = "PCoA plot of bray curtis distance")
 
 variability_table = function(cca){
   chi = c(cca$tot.chi, cca$CCA$tot.chi, cca$CA$tot.chi)
@@ -81,21 +81,21 @@ points.aw <- as.data.frame(pcoa.aw$points)
 colnames(points.aw) <- c("x", "y", "z") 
 eig.aw <- pcoa.aw$eig
 points.aw <- cbind(points.aw, design[match(rownames(points.aw), rownames(design)), ])
-p6 <- ggplot(points.aw,aes(x = x,y = y, color = status, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.9, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.aw[1] / sum(eig.aw), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.aw[2] / sum(eig.aw), digits=4), "%)", sep=""),title = "PCoA plot of weighted unifrac distance") + scale_color_manual(values = c("lightgoldenrod2","orchid"))
+p6 <- ggplot(points.aw,aes(x = x,y = y, color = status, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.8, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.aw[1] / sum(eig.aw), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.aw[2] / sum(eig.aw), digits=4), "%)", sep=""),title = "PCoA plot of weighted unifrac distance") + scale_color_manual(values = c("lightgoldenrod2","orchid"))
 
 pcoa.au <- cmdscale(unweighted, k=3, eig=T)
 points.au <- as.data.frame(pcoa.au$points)
 colnames(points.au) <- c("x", "y", "z") 
 eig.au <- pcoa.au$eig
 points.au <- cbind(points.au, design[match(rownames(points.au), rownames(design)), ])
-p7 <- ggplot(points.au,aes(x = x,y = y, color = status, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.9, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.au[1] / sum(eig.au), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.au[2] / sum(eig.au), digits=4), "%)", sep=""),title = "PCoA plot of unweighted unifrac distance") + scale_color_manual(values = c("lightgoldenrod2","orchid"))
+p7 <- ggplot(points.au,aes(x = x,y = y, color = status, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.8, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.au[1] / sum(eig.au), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.au[2] / sum(eig.au), digits=4), "%)", sep=""),title = "PCoA plot of unweighted unifrac distance") + scale_color_manual(values = c("lightgoldenrod2","orchid"))
 
 pcoa.ab <- cmdscale(bray, k=3, eig=T)
 points.ab <- as.data.frame(pcoa.ab$points)
 colnames(points.ab) <- c("x", "y", "z") 
 eig.ab <- pcoa.ab$eig
 points.ab <- cbind(points.ab, design[match(rownames(points.ab), rownames(design)), ])
-p8 <- ggplot(points.ab,aes(x = x,y = y, color = status, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.9, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.ab[1] / sum(eig.ab), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.ab[2] / sum(eig.ab), digits=4), "%)", sep=""),title = "PCoA plot of bray curtis distance") + scale_color_manual(values = c("lightgoldenrod2","orchid"))
+p8 <- ggplot(points.ab,aes(x = x,y = y, color = status, shape = sample_type)) + theme_bw() + geom_point(alpha = 0.8, size = 3) + labs(x=paste("PCoA 1 (", format(100 * eig.ab[1] / sum(eig.ab), digits=4), "%)", sep=""), y=paste("PCoA 2 (", format(100 * eig.ab[2] / sum(eig.ab), digits=4), "%)", sep=""),title = "PCoA plot of bray curtis distance") + scale_color_manual(values = c("lightgoldenrod2","orchid"))
 
 dis_tbl <- as.dist(bray,diag = FALSE,upper = FALSE)
 adonis_tbl <- adonis(dis_tbl~status,data = design,permutations = 999)
