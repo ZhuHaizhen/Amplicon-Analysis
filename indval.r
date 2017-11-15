16adffdf8ec020a9be180b0ea37896b60923b37f
ZhuHaizhen <32768294+ZhuHaizhen@users.noreply.github.com>
2017-11-16 07:59:05 +0800

    171116

--- indval.r
+++ indval.r
@@ -9,6 +9,10 @@ bind.rel <- cbind(ind.rel,ind[,0])
 f <- ind[0,]
 colnames(bind.rel) = colnames(f)
 
-grp <- c(rep(1,21),rep(2,18),rep(3,16),rep(4,14),rep(5,17),rep(6,14),rep(7,18),rep(8,10))
-indval <- multipatt(bind.rel,grp,func = "IndVal",duleg = TRUE,control = how(nperm = 999))
-summary(indval,indvalcomp = TRUE)
+grp.cave <- c(rep(1,21),rep(2,18),rep(3,16),rep(4,14),rep(5,17),rep(6,14),rep(7,18),rep(8,10))
+indval.cave <- multipatt(bind.rel,grp.cave,func = "IndVal",duleg = TRUE,control = how(nperm = 999))
+summary(indval.cave,indvalcomp = TRUE)
+
+grp.samp <- c(rep(1,6),rep(2,6),rep(3,6),rep(4,3),rep(1,4),rep(2,5),rep(3,6),rep(4,3),rep(1,5),rep(2,5),rep(3,5),rep(4,1),rep(1,4),rep(2,4),rep(3,4),rep(4,2),rep(1,5),rep(2,5),rep(3,4),rep(4,3),rep(1,4),rep(2,4),rep(3,4),rep(4,2),rep(1,5),rep(2,5),rep(3,5),rep(4,3),rep(1,3),rep(2,2),rep(3,3),rep(4,2))
+indval.samp <- multipatt(bind.rel,grp.samp,func = "IndVal",duleg = TRUE,control = how(nperm = 999))
+summary(indval.samp,indvalcomp = TRUE)
