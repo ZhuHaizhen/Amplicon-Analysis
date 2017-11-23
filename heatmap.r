e2b8546cc5f8ec5fbd8a9c14880779df57da4739
ZhuHaizhen <32768294+ZhuHaizhen@users.noreply.github.com>
2017-11-23 08:27:09 +0800

    171123

--- heatmap.r
+++ heatmap.r
@@ -1,8 +1,16 @@
-library(readr)
 library(tidyr)
 library(ggplot2)
-dataset <- read_csv("E:/lab/A_Cave/raw_data_Zhang/171017/L2.csv")
-phylum<-gather(dataset, key = Cave, value = Abundance, - Phylum)
-p<-ggplot(phylum,aes(x=Cave, y=Phylum)) + theme_classic() + geom_tile(aes(fill=Abundance)) + theme(axis.text.x = element_text(angle = 45,hjust = 1,vjust = 1)) + theme(axis.text.y = element_text(face="italic")) + theme(axis.text.y = element_text(size = 8)) + theme(axis.title = element_text(size = 10)) + theme(legend.title = element_text(size = 10)) + scale_fill_gradient(low = "lightskyblue",high = "lightsteelblue4")
-p
-ggsave(p,filename = "heatmap.pdf",width = 10, height = 10,units = c("cm"))
+
+l3 <- read.table("L3.in.txt",header=T, sep="\t")
+class <- gather(l3, key = Cave, value = Abundance, - Class)
+p3 <- ggplot(class,aes(x=Cave, y=Class)) + theme_classic() + geom_tile(aes(fill=Abundance),color = "white") + theme(axis.text.x = element_text(angle = 45,hjust = 1,vjust = 1)) + theme(axis.text.y = element_text(face="italic")) + theme(axis.text.y = element_text(size = 8)) + theme(axis.title = element_text(size = 10)) + theme(legend.title = element_text(size = 10)) + scale_fill_gradient(low = "lightblue",high = "lightcoral")
+
+l2 <- read.table("L2.in.txt",header=T, sep="\t")
+phylum<-gather(l2, key = Cave, value = Abundance, - Phylum)
+p2 <- ggplot(phylum,aes(x=Cave, y=Phylum)) + theme_classic() + geom_tile(aes(fill=Abundance),color = "white") + theme(axis.text.x = element_text(angle = 45,hjust = 1,vjust = 1)) + theme(axis.text.y = element_text(face="italic")) + theme(axis.text.y = element_text(size = 8)) + theme(axis.title = element_text(size = 10)) + theme(legend.title = element_text(size = 10)) + scale_fill_gradient(low = "lightblue",high = "lightcoral")
+
+l5 <- read.table("L5.in.txt",header=T, sep="\t")
+family <- gather(l5, key = Cave, value = Abundance, - Family)
+p5 <- ggplot(family,aes(x=Cave, y=Family)) + theme_classic() + geom_tile(aes(fill=Abundance),color = "white") + theme(axis.text.x = element_text(angle = 45,hjust = 1,vjust = 1)) + theme(axis.text.y = element_text(face="italic")) + theme(axis.text.y = element_text(size = 8)) + theme(axis.title = element_text(size = 10)) + theme(legend.title = element_text(size = 10)) + scale_fill_gradient(low = "lightblue",high = "lightcoral")
+
+ggsave(p3,filename = "heatmap_class.pdf",width = 10,height = 8)
